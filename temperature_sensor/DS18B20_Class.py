91d0177d36647d4316a426ed65623fabfdb04dbd
caiminkeavney <caiminkeavney@gmail.com>
2026-03-03 16:21:32 +0000

    Create DS18B20_Class.py

---
+++ temperature_sensor/DS18B20_Class.py
@@ -0,0 +1,22 @@
+class DS18B20:
+    """Interface to the DS18B20 thermometer"""
+
+    def __init__(self, address: ds18x20.DS18X20(onewire.OneWire(one_wire_pin))) -> None:
+        
+
+
+
+
+
+
+
+
+
+
+
+    def 
+
+
+
+
+
