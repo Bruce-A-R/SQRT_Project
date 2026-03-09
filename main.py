ab7ec2837da6ff6fa68e4d42e3495999c77f4932
caiminkeavney <caiminkeavney@gmail.com>
2026-03-09 21:03:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-"""
-main.py
-
-Main loop to run on SQRT onboard computer
-
-
-
-"""
-
-
 from ms5611 import MS5611
 from ds18b20 import DS18B20
 from mlx90640 import MLX90640
+from gps import GPS
+from comms import Comms
 
+import os
 import time
 import machine
 
 from machine import SPI, Pin
 import sdcard, os
 
-
-
-
-# setup tasks: files on sd card: 
-
-# SD CARD SETUP:
+# -----------------------------SD CARD SETUP--------------------------------
 SPI_BUS = 0
 SCK_PIN = 2
 MOSI_PIN = 3
@@ -31,73 +19,73 @@ MISO_PIN = 4
 CS_PIN = 5
 SD_MOUNT_PATH = '/sd'
 
-file_list = ['sd/trigger_log.txt', 'sd/ms5611_log.txt', 'sd/ds18b20_log.txt', 'sd/mlx90640_log', 'sd/gps_log']
+file_list = ['/sd/trigger_log.txt', '/sd/ms5611_log.txt', '/sd/ds18b20_log.txt', '/sd/mlx90640_log.txt', '/sd/gps_log.txt']
    
 try:
     spi = SPI(SPI_BUS,sck=Pin(SCK_PIN), mosi=Pin(MOSI_PIN), miso=Pin(MISO_PIN))
     cs = Pin(CS_PIN)
     sd = sdcard.SDCard(spi, cs)
+    
     # Mount microSD card
     os.mount(sd, SD_MOUNT_PATH)
     
+except:
+    print('SD card Pin Error')
+
+#------------------------Textfile Initialisation----------------------------
+try:
+    for file in file_list:
+        os.remove(file)
+    
+    
     with open(file_list[0], "w") as file:                      # trigger log file
-        write("Timestamp (s), Trigger, Trigger Condition \n")
+        file.write("Timestamp (s), Trigger, Trigger Condition \n")
     
     with open(file_list[1], "w") as file:                      # pressure file
-        write("Timestamp (s), Pressure (centi mbar), Temperature (centi Celsius) \n")
+        file.write("Timestamp (s), Temperature (Celsius), Pressure (mbar) \n")
     
     with open(file_list[2], "w") as file:                      # thermometer file
-        write("Timestamp, Temp (deg)")
+        file.write("Timestamp, Temp (deg)\n")
     
     with open(file_list[3], "w") as file:                     # thermal sensor file
-        write("\n")
+        file.write("\n")
     
     with open(file_list[4], "w") as file:                     # gps file
-        write("Timestamp (s), Lat (deg), Lon (deg), Alt (m)\n")
+        file.write("Timestamp (s), Lat (deg), Lon (deg), Alt (m)\n")
 
-except: print("ruh roh SD card error")
+except: print("SD card error")
 
-# pressure sensor:
+# --------------------------- Sensor Setup -------------------------------
 try:
-    pressure_sensor = MS5611(i2c_bus=1, sda_pin=6, scl_pin=7)    
-
+    pressure_sensor = MS5611(i2c_bus=0, sda_pin=8, scl_pin=9)
 except:
     pressure_sensor = None
     print("Pressure Sensor Error")
 
-# temp sensor:
 try:
     temp_sensor = DS18B20(pin=21)
 except:
     temp_sensor = None
     print("Temperature Sensor Error")
 
-# thermal sensor
 try:
     frame_taker = MLX90640(i2c_bus=1, sda_pin=14, scl_pin=15)
 except:
     frame_taker = None
-    print("MLX90640 Failure")
-
-# gps
+    print("MLX90640 Sensor Error")
+    
 try:
-    #gps_sensor = SQRTGPS(UART_bus = 0, TX =, RX = ) #UART(0, baudrate =9600 , tx=Pin(12), rx=Pin(13))
+    gps_sensor = GPS(uart tx_pin = , rx_pin = )
+    
 except:
-    gps_sensor = None 
-    print("GPS Failure")
+    gps_sensor = None
+    print("GPS Sensor Error")
 
 
-# antenna
-
-
-
-# servo motor
-
-
-
-# DATA COLLECTION LOOP:
-
+# ------------------- Data Acquisition -------------------------------------
+counter = 0
 while True:
+
     if pressure_sensor:
         T_val = pressure_sensor.read_adc('T')
         P_val = pressure_sensor.read_adc('P')
@@ -106,27 +94,33 @@ while True:
         
         if T_val and P_val:
             T_E, P = pressure_sensor.compute_pressure(T_val, P_val)
-            print(T_E, P)
-            
+
         else:
             print("MS5611 failed")
-        
-        pressure_sensor.log_pressure()
-        
+        try:
+            pressure_sensor.log_pressure(file_list[1])
+        except: print('Pressure not logged')
         
     if temp_sensor:
-        T_int = temp_sensor.read_temp()
         
-        if T_int is not None:
-            print(f"T_int:{T_int}")
-            
-        else:
-            print("DS18B20 failed")
-        
-        temp_sensor.log_temp()
+        try:
+            temp_sensor.log_temp(file_list[2])
+        except: print('Temp not logged')
+
     
-
     if frame_taker:
-        frame_taker.mlx_log()
+        frame_taker.mlx_log(file_list[3])
 
-    time.sleep(5)
+    if gps_sensor:
+        gps_sensor.gps_log(file_list[4])
+        
+#-------------------------- Data Downlink-----------------------------------------------------
+    if counter%10==0 and counter%3==0:
+       Comms.data_packet()
+  
+    elif counter%10==0:
+       Comms.telem_packet()
+
+    counter += 1
+       
+       
