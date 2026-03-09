d2781c14723022a73041b4b5866eb48bf6357add
Bruce-A-R <ritter.a.bruce@gmail.com>
2026-03-09 14:11:35 +0000

    Update sdcard_testing.py

--- sdcard_testing.py
+++ sdcard_testing.py
@@ -20,7 +20,9 @@ MOSI_PIN = 3
 MISO_PIN = 4
 CS_PIN = 5
 SD_MOUNT_PATH = '/sd'
-FILE_PATH = 'sd/sd_test_file.txt'
+FILE_PATH = 'sd/sd_test_file2.txt'
+
+file_list = ['sd/sd_test_2.txt', 'sd/sd_test_3.txt']
 
 try:
     # Init SPI communication
@@ -32,19 +34,31 @@ try:
     # List files on the microSD card
     print(os.listdir(SD_MOUNT_PATH))
     
+    for file_path in file_list:
+        
+        with open(file_path, "w") as file:
+            file.write("Testing microSD Card writing in loop \n")
+        
+        with open(file_path, "a") as file:
+            file.write("testing appending a line :) \n")
+    
     # Create new file on the microSD card
-    with open(FILE_PATH, "w") as file:
+    #with open(FILE_PATH, "w") as file:
         # Write to the file
-        file.write("Testing microSD Card \n")
+     #   file.write("Testing microSD Card \n")
+        
+    #with open(FILE_PATH, "a") as file:
+     #   file.write("I still love squirtle :) \n")
         
     # Check that the file was created:
-    print(os.listdir(SD_MOUNT_PATH))
+        print(os.listdir(SD_MOUNT_PATH))
     
     # Open the file in reading mode
-    with open(FILE_PATH, "r") as file:
+    #with open(FILE_PATH, "r") as file:
         # read the file content
-        content = file.read()
-        print("File content:", content)  
+     #   content = file.read()
+      #  print("File content:", content)  
     
+    os.umount(SD_MOUNT_PATH)
 except Exception as e:
     print('An error occurred:', e)
