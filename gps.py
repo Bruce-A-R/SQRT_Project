e526fa443a9f41f05afb9ea2eb5ec407e1f55031
Bruce-A-R <ritter.a.bruce@gmail.com>
2026-03-15 19:27:15 +0000

    Update and rename gps_class.py to gps.py

--- gps.py
+++ gps.py
@@ -1,20 +1,60 @@
+"""
+gps.py
+
+script with SQTGPS class in it that should initialize and read from gps
+
+when called in the main loop, it should reutrn the values we want (lat, lon, altitude, timestamp, hdop) as a string
+so we can write it to a file on the sd card
+
+BR 15/3
+"""
+
+from machine import UART, Pin
+import time
+
 class SQTGPS:
     """gps class SQTGPS, used to read out gps
 
-    should read gps using set port and pins and then write reults to text file
-
-    9/3/16 Bruce (this version likely wont work but we'll test it tomorrow)
+    reads and returns the values we want from a GGA sentence
     """
 
 
-    def __init__(self, uart = 0, baudrate = 9600, tx_pin = 12, rx_pin = 13):
+    def __init__(self, uart_bus, baudrate, tx_pin, rx_pin):
         """Funciton to initialze and connect to sensor"""
-        self.gps = UART(uart, baudrate = baudrate, tx = Pin(tx_pin), rx = Pin(rx_pin))
-        self.gps.init(baudrate, bits = 8, parity = None, stop = 1)                # taken from setup tasks 
+        
+        self.gps = UART(uart_bus, baudrate = baudrate, tx = Pin(tx_pin), rx = Pin(rx_pin))
+        
+        #try:
+        #    self.devices = self.UART.scan()
+        #    print(devices)
+        #except Exception as e:
+        #    print(f"exception to just fucking scanning for devices: {e}")
+            
+        
+       # try:
+       #     self.gps.init(baudrate, bits = 8, parity = None, stop = 1)                # taken from setup tasks 
+        #except Exception as e:
+        #    print(f"init exception: {e}")
+        
+        timeout = 10000  
+        start = time.ticks_ms()
+        
+        try:
+            while time.ticks_diff(time.ticks_ms(), start) < timeout:
+                if self.gps.any():
+                    print("found gps")
+                    break                                                           # don't continue the loop if gps found
+        except Exception as e:
+            print(f" did not find GPS, and also: {e}")
+            
 
+        #while time.ticks_diff(time.ticks_ms(), start) < timeout:
+        #    if self.gps.any():
+        #        return
+        #
+        #raise RuntimeError("No GPS Found")
 
-
-    def _angle_reader(self, angle_string, hemisphere):
+    def _angle_reader(self, angle_str, hemisphere):
         """Takes an angle as a string in DDMM.MMMMM or 
         DDDMM.MMMMM format and return it as a float in decimal degrees
            
@@ -23,17 +63,17 @@ class SQTGPS:
         """
 
         if len(angle_str) == 9:  #was 10:         # for DDMM.MMMMM format
-            minutes = float(angle_str[2:10]) / 60
+            minutes = float(angle_str[2:9]) / 60
             degs = int(angle_str[0:2])
             
             ang = degs + minutes
-            ang = sign_angle(ang, hemisphere)
+            ang = self.sign_angle(ang, hemisphere)
             
             return ang
         
         elif len(angle_str) == 10:   #was 11:       # for DDDMM.MMMMM format
-            minutes = float(angle_str[2:11]) / 60
-            degs = int(angle_str[0:2])
+            minutes = float(angle_str[3:10]) / 60
+            degs = int(angle_str[0:3])
             
             ang = degs + minutes
             ang = sign_angle(ang, hemisphere)
@@ -49,7 +89,7 @@ class SQTGPS:
 
     def _sign_angle(self, angle, hemisphere):
         """Assigns a sign to the angle based on inputted hemisphere"""
-        if hemisphere == 's' or hemisphere == 'W':
+        if hemisphere == 'S' or hemisphere == 'W':
             angle = angle * -1
 
         return angle
@@ -63,12 +103,17 @@ class SQTGPS:
         is of the requested type)
         """
         requested_sequence = None
+        
 
-        while port.any():
-            data = port.readline()
+
+        
+        while self.gps.any():
+            data = self.gps.readline()
+            print(data)
             
             try:
                 sentence = data.decode('ascii')
+                print(sentence)
                 sentenceid = sentence[3:6]
                 if sentenceid == 'GGA':              # returns the sentece only if it is GGA
                     requested_sequence = sentence
@@ -76,8 +121,10 @@ class SQTGPS:
                 print("unicode error, retrying")
             
         if requested_sequence:
+            print(requested_sentence)
             return requested_sequence
         else:
+            print("no sentence")
             return None
 
     def _parse_sequence(self, sequence):
@@ -102,8 +149,8 @@ class SQTGPS:
         split_sentence = sequence.split(',')
         
         # converting lat and lon into degrees:
-        lat = angle_reader(split_sentence[2], split_sentence[3])
-        lon = angle_reader(split_sentence[4], split_sentence[5])
+        lat = self.angle_reader(split_sentence[2], split_sentence[3])
+        lon = self.angle_reader(split_sentence[4], split_sentence[5])
         
         # assigning parts of the sequence to the dictiory keys
         if len(split_sentence[1]) != 0:
@@ -113,7 +160,7 @@ class SQTGPS:
     
         mydict['latitude'] = lat
         mydict['longitude'] = lon
-        
+        mydict['hdop'] = split_sentence[8]
         if len(split_sentence[9]) != 0:
             mydict['altitude'] = split_sentence[9]
         else: 
@@ -130,30 +177,35 @@ class SQTGPS:
         
         return mydict
 
-    def read_gps(self, file_name):
+    def gps_log(self):
         """Funciton to write the values we want from the read sequence to a text file. First it reads in data, 
-        then it should save it all to a text file. It will loop reading from the GPS until it gets a GGA sequence
-        Input: path to desired text file (should be on sd card) (string)
-        Output: writes to that file
-        """
+        then it should return the wanted values as a string to be saved to the sd card
+        which will happen in the main function"""
 
         GGA = False
         gga_count = 0
+        
         # reading and calibrating GPS data: 
-        while GGA == False:
+        while GGA_count_done == False:
             
             time.sleep(1)
-            sentence = listen_for_sequence(gps, 'GGA')
+            sentence = self._listen_for_sequence()
             
             if sentence is not None:
                 # parsing sequence:
-                dictionary = parse_sequence(sentence)
+                dictionary = self.parse_sequence(sentence)
     
-                # writing result string to the file
-                with open(file_name, "a") as file:
-                    file.write(f"{dictionary["timestamp"]},{dictionary["latitude"]},{dictionary["longitude"]},{dictionary["altitude"]}\n")
+                # making and returning result string
+                
+                print(f"{dictionary["timestamp"]},{dictionary["latitude"]},{dictionary["longitude"]},{dictionary["altitude"]}, {dictionary["hdop"]}\n")
+                return f"{dictionary["timestamp"]},{dictionary["latitude"]},{dictionary["longitude"]},{dictionary["altitude"]}, {dictionary["hdop"]}\n"
+                    
+                
+                
                 
                 gga_count += 1
-            
-            if gga_count == 10:
+     
+            if gga_count_done == 10:
                 GGA = True     # setting GGA flag so the listening loop can stop
+                return f"{time.time()}, None, None, None, None, None \n"
+                
