42cf200826a4671b38efd5349b2782e899fae954
Bruce-A-R <ritter.a.bruce@gmail.com>
2026-03-30 12:37:50 +0100

    Update triggering_algorithm.py

--- operations_classes/triggering_algorithm.py
+++ operations_classes/triggering_algorithm.py
@@ -1,75 +1,73 @@
-"""
-triggering_algorithm.py
-
-script that has the SQTtrigger class is it that we use to run trigger checks
-
-atm we are calling two functions from it in main_v3
-"""
-
-
-
 class SQTtrigger:
     """SQRT Triggering Algorithm class
 
-    BR version as of 11/3
+    current use (30/3):
+    will parse out values from one housekeeping file that has data from both the gps and pressure sensor (as well as temp sensors),
+    and only one loop timestamp
+    -BR
     
-    Used to check for contitions to trigger our servo motor (to open the valve)
+    Used to check for contitions to trigger our servo motor (to open the valve), and then do a longer run of thermal photos
     How to use:
     1. Have a flag in the main tuppersat loop that triggering has or hasnt happened,
     and only run the check if it hasn't happened yet
     2. trigger_check will return both True/False trigger and the condition type of triggering
     
     """
-    
-    def __init__(self, pressure_file, gps_file, trigger_file):
-        self.pressure_dict, self.gps_dict = self._parse_files_triggering(pressure_file, gps_file)
 
-    def _parse_files_triggering(self, pressure_file, gps_file):
+    def __init__(self):
+        print("trigger algo ready")
+
+    def _parse_files_triggering(self,  housekeeping_file): #pressure_file, gps_file):
         """Function to parse saved text files of pressure and altitude into dict objects
             for use in the triggering algorithm.
-            Inputs: names to files
+            Inputs: name of housekeeping file
             Outputs: dicts for timestamped pressure data and timestamped altitude data
         """
-        # first parsing pressure file: the values go timestamp, pressure in cetnimbar, temperature in centiC
+        # the housekeeping data is now all in one file, but I'll still read into two seperate dictionaries just to save
+        # the time of editing the code 
     
         pressure_dict = {
             'timestamp' : [],    # ms or mabe s actually?
             'pressure' : [],     # units of mbar
             'temperature' : [],     # C
         }
-    
-        with open(pressure_file, 'r') as file:
-            lines = file.readlines()
-            
-            for i, line in enumerate(lines): 
-                if i > 0:                      # to skip the first line that is saying collumn key
-                    values = line.split(",")
-                    
-                    pressure_dict['timestamp'].append(int(values[0]))
-                    pressure_dict['pressure'].append(float(values[1]) / 100)
-                    pressure_dict['temperature'].append(float(values[2][:-2]) / 100)     # leave off the last 2 characters in the string cuz they are all "\n"
-    
-        #now gps file:
-    
+        
         gps_dict = {
             "timestamp" : [],    # ms
             "altitude": []       # m
         }
-    
-        with open(gps_file, 'r') as file:
+        
+        with open(housekeeping_file, 'r') as file:
             lines = file.readlines()
-    
-            for i, line in enumerate(lines):
-                if i > 0:
-                    values = line.split(",")
-                    gps_dict["timestamp"].append(float(values[0]))
-                    gps_dict["altitude"].append(float(values[3]))
-    
-                    
-            #gps_dict["timestamp"].append(float(values[0]))
-            #gps_dict["altitude"].append(float(values[3]))  # leave off the last 2 characters in the string cuz they are all "\n"
+        
+        if len(lines) > 1:
+            line = lines[-1]                    # getting last line of data that is not collumn key
+            values = line.split(",")
+            
+            try:
+                pressure_dict['timestamp'].append(float(values[0]))
+                gps_dict['timestamp'].append(float(values[0]))
+            except:
+                pressure_dict['timestamp'].append(time.time())
+                gps_dict['timestamp'].append(time.time())
+            
+            try:
+                pressure_dict['temperature'].append(float(values[1]))
+            except:
+                pressure_dict['temperature'].append(999) # appending 999 for pressure and temperautre if nothing there
+                
+            try:
+                pressure_dict['pressure'].append(float(values[2]))
+            except:
+                pressure_dict['pressure'].append(999)
+                
+            try:
+                gps_dict["altitude"].append(float(values[7]))
+            except:
+                gps_dict["altitude"].append("NaN")
         
         return pressure_dict, gps_dict
+    
     
     def _check_pressure(self, pressure_value):
         """Function to check if a pressure value is above a set threshold
@@ -77,17 +75,17 @@ class SQTtrigger:
             Output: True/False
         """
         print(f"reading pressure: {pressure_value}")
-        if pressure_value <= 35:     # assuming pressure value in units of mbar
+        if float(pressure_value) <= 200.0: # changed from 35.0 for testing 35.0:     # assuming pressure value in units of mbar
             return True
         else : return False
     
-    def _check_altitude(self, altitude_value):
+    def _check_altitude(self,altitude_value):
         """Function to check if an altitude value is above a set threshold
             Input: altitude value
             Output: True/False
         """
     
-        if altitude_value >= 22000:     # assuming altitude value in units of m, currenlty set to above 22 km but we could make it 23 km
+        if float(altitude_value) >= 23000.0:     # assuming altitude value in units of m, currenlty set to above 23 km
             return True
         else: return False
     
@@ -97,27 +95,31 @@ class SQTtrigger:
             Inputs: dict of timestamps and altitude values
             Output: True/False
         """
-        if altitudes_dict["altitude"][-1] > 2000:     # assuming altitude in km, first check is we'rve above 2000km 
+        if float(altitudes_dict["altitude"][-1]) > 2000.0:     # assuming altitude in km, first check is we'rve above 2000km 
             
-            decreases_list = []
-            
+            r_altitudes = altitudes_dict["altitude"][::-1]      # reversing list to start with latest value
+
+            decreases_count = 0
             try:
-                for i in range(len(altitudes_dict["altitude"] - 1)):
-                    #if altitude_dict["altitude"][i+1] > timestamped_altitude_dict["altitude"][i]: # wrong sequence I think its backwards
-                    if altitudes_dict["altitude"][len(altitudes_dict["altitude"] - i)] << altitudes_dict["altitude"][len(altitudes_dict["altitude"] - i + 1)]: 
-                        decreases_list.append("d")
-            
-                if len(decreases_list) >= 10:    # arbitrary if 10 altitude readings of decreasing altitude, TBC
+                for i in range(len(r_altitudes) - 1):
+                    if r_altitudes[i] < r_altitudes[i + 1]:
+                        decreases_count += 1
+
+                if decreases_count >=3:
+                    print(f"True. {decreases_count}")
                     return True
-                else: return False
-                    
-            except:      # probably need to specify exceptions (ie: list to short, list doesn't exist)
+                else:
+                    print(f"False. {decreases_count}")
+                    return False
+            
+            except Exception as e:           #if list to short, just return false but also print error anyways just in case
+                print(f"Exception in falling trigger check: {e}")
                 return False
-    
-        else: 
+        
+        else:
             return False
     
-    def _check_pressure_sensor_failure(self, pressure_dict, altitude_dict):
+    def _check_pressure_sensor_failure(self,pressure_dict, altitude_dict):
         """Function to check if there is something wrong with the pressure sensor
             Inputs: dictionary of timestamped pressure sensor readings, dictionary of timestamped altitude readings
             Outputs: True/False
@@ -138,8 +140,6 @@ class SQTtrigger:
     
         if len(pressure_dict["pressure"]) == 0:     # never collected pressures
             return True
-        elif len(nones_list) >= len(pressure_dict["pressure"]) / 2:     # more than half the pressures are Nones
-            return True
         elif pressure_dict["pressure"][0] == pressure_dict["pressure"][-1]:     # basic and not done check that the values r the same
             return True
         else: 
@@ -148,42 +148,71 @@ class SQTtrigger:
     
     # actual trigger check:
     
-    def trigger_check(self): 
+    def trigger_check(self, pressure_dict, gps_dict): 
         """Function to run triggering check
             Priority: check pressure
             Secondary check: check altiude and if pressure sensor is bugging
             Ugly third check: check if tuppersat is descending
     
-            should save a timestamped line of trigger check result and condition to
-            the trigger log file
-            
-            should also return the check 
+            Inputs: parsed dict object from pressure file, parsed dict object from gps file
+            Outputs: check (boolean), condition (string), pressure value used (float), alt value used (float)
         """
-    
         
-        pressure_dict, gps_dict = parse_files_triggering(pressure_file, gps_file)
+        check = False
+        condition = "None"
+        error = "None"
 
-
+        if len(pressure_dict["pressure"]) == 0:
+            presure = 'None'
+            
+            if len(gps_dict["altitude"])==0:
+                altitude = "None"
+            else: altitude = gps_dict['altitude'][-1]
+            
+            check = False
+            condition = "None"
+            print(f'trigger returns: {check},{condition}, {pressure}, {altitude}')
+            return check, condition, pressure, altitude, error
+            
+            
+            
+        if len(gps_dict["altitude"])==0:
+            altitude = "None"
+            
+            if len(pressure_dict["pressure"]) == 0:
+                pressure = "None"
+            else: pressure = pressure_dict['pressure'][-1]
+            
+            check = False
+            condition = "None"
+            print(f'trigger returns: {check}, {condition}, {pressure}, {altitude}')
+            return check, condition, pressure, altitude, error
+            
         #pressure_flag = check_pressure(self.pressure_dict['pressure'][-1])
         #print(pressure_flag)
-        print(self.pressure_dict['pressure'][-1])
-        print(self.gps_dict['altitude'][-1])
+        #print(pressure_dict['pressure'][-1])
+        #print(gps_dict['altitude'][-1])
+        
+        pressure = pressure_dict['pressure'][-1]
+        altitude = gps_dict['altitude'][-1]
+        
         try:
-            if check_pressure(self.pressure_dict['pressure'][-1]) == True:      # pressure trigger
+            if self._check_pressure(pressure) == True:      # pressure trigger
                 check = True
                 condition = "G"
-            elif check_altitude(self.gps_dict['altitude'][-1]) == True and check_pressure_sensor_failure(pressure_dict, gps_dict) == True :
+            elif self._check_altitude(altitude) == True and self._check_pressure_sensor_failure(pressure_dict, gps_dict) == True :
                 check = True
                 condition = "B"
-            elif check_falling(self.gps_dict) == True:
+            elif self._check_falling(gps_dict) == True:
                 check = True
                 condition = "U"
             else:
                 check = False
                 condition = "None"
-        except:    
-            print("exception in triggering check")
+        except Exception as e:    
+            print(f"exception in triggering check: {e}")
             check = False
+            error = f"exception in triggering check: {e}"
             condition = "None"
             
         # not saving directly to file anymore, instead returning check and condition type
@@ -192,11 +221,12 @@ class SQTtrigger:
         #    file.write(f"{time.time()}, {check}, {condition}")
         
         #tests:
-        print(check)
-        print(condition)
+        print(f'check: {check}')
+        print(f'condition: {condition}')
+        print(f'current pressure: {pressure}')
+        print(f'current altitude: {altitude}')
 
     
         
-        return check, condition
+        return check, condition, pressure, altitude, error
     
-        return check
