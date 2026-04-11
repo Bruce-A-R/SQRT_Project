b6b4e57594bf033941296143d45d0912278245d4
Bruce-A-R <ritter.a.bruce@gmail.com>
2026-04-11 17:28:36 +0100

    Create _utils.py

---
+++ SQRT Software/_utils.py
@@ -0,0 +1,21 @@
+"""
+_utils.py
+
+Used in _tuppersat_radio.py and _rhserial_radio.py
+Contains Counter function
+"""
+
+def Counter(start=0x00, modulo=None):
+    """Callable that counts from start every time it is called."""
+    _count = start
+
+    def _counter():
+        nonlocal _count
+        idx = _count
+        # update counter
+        _count += 1
+        if modulo:
+            _count %= modulo
+        return idx
+
+    return _counter
